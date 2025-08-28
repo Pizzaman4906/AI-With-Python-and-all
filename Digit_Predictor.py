88976ab64e3a6ed600686a15bf3deac5ec351403
Pizzaman4906 <pizzamango1212@gmail.com>
2025-08-28 12:41:40 +0530

    Add files via upload

---
+++ Digit_Predictor.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import fetch_openml
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import matplotlib.pyplot as plt
+
+mnist = fetch_openml('mnist_784', version=1)
+
+x = mnist['data'] / 255.0
+y = mnist['target'].astype(int)
+
+x_train, x_test, y_test = train_test_split(X, Y, test_size = 0.1, random_state=42)
+
+model = LogisticRegression(max_iter = 10000)
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+accuracy = metrics.accuracy_score(y_test, y_pred)
+print(f"Test accuracy: {accuracy}")
+
+for i in range(5):
+    plt.imshow(x_test.iloc[i].values.reshape(28, 28), cmap=plt.cm.binary)
+    plt.title(f"Predicted: {y_pred[i]}, Actual {y_test.iloc[i]}")
+    plt.show()
